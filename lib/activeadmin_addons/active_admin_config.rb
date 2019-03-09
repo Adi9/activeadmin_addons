0f53a9c54b54ce52f2277b52ad756e461b33a457
Adi9 <ady.ro.mail@gmail.com>
2019-03-09 17:13:11 +0200

    - fix broken add_classes_to_body

--- lib/activeadmin_addons/active_admin_config.rb
+++ lib/activeadmin_addons/active_admin_config.rb
@@ -1,8 +1,14 @@
 class ActiveAdmin::Views::Pages::Base
-  alias_method :original_add_classes_to_body, :add_classes_to_body
+  # alias_method :original_add_classes_to_body, :add_classes_to_body
 
-  def add_classes_to_body
-    original_add_classes_to_body
-    @body.set_attribute "data-default-select", ActiveadminAddons.default_select
+  # def add_classes_to_body
+  #   original_add_classes_to_body
+  #   @body.set_attribute "data-default-select", ActiveadminAddons.default_select
+  # end
+  alias_method :original_build, :build
+
+  def build(*args)
+    original_build(args)
+    body.set_attribute "data-default-select", ActiveadminAddons.default_select
   end
 end
